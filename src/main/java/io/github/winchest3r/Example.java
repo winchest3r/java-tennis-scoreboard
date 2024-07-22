edf690ff09c8de0a2145ef3d4b6fa34d5a05c7d1
Evgenii Uglanov <uglanoveugene@gmail.com>
2024-07-22 19:51:27 +0500

    Init maven webapp

---
+++ src/main/java/io/github/winchest3r/Example.java
@@ -0,0 +1,7 @@
+package io.github.winchest3r;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("42!");
+    }
+}
---
+++ src/test/java/io/github/winchest3r/ExampleTest.java
@@ -0,0 +1,12 @@
+package io.github.winchest3r;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+    @Test
+    void exampleTest() {
+        assertEquals(21 * 2, 42);
+    }
+}
