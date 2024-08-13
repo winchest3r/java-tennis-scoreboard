a35aa8f7e8af2bb800af9f583250be3dae790e39
Evgenii Uglanov <uglanoveugene@gmail.com>
2024-08-13 15:41:01 +0500

    Fix set adding

--- src/main/java/io/github/winchest3r/controller/Match.java
+++ src/main/java/io/github/winchest3r/controller/Match.java
@@ -1,7 +1,7 @@
 package io.github.winchest3r.controller;
 
 import jakarta.inject.Named;
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.view.ViewScoped;
 import jakarta.faces.context.FacesContext;
 
 import java.util.UUID;
@@ -9,14 +9,18 @@ import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.io.Serializable;
 
 import io.github.winchest3r.service.ScoreboardService;
 import io.github.winchest3r.model.Playset;
 import io.github.winchest3r.model.Game;
+import io.github.winchest3r.util.ScoreboardUtil;
 
 @Named(value = "match")
-@RequestScoped
-public class Match {
+@ViewScoped
+public class Match implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     /** Scoreboard service. */
     private static ScoreboardService scoreboardService =
         new ScoreboardService();
@@ -29,9 +33,6 @@ public class Match {
 
     /** Playsets related to match. */
     private List<Playset> sets;
-
-    /** Set that is ongoing. */
-    private Playset currentSet;
 
     /** Constructor. */
     public Match() { }
@@ -76,16 +77,31 @@ public class Match {
     }
 
     /**
+     * Get UUID.
+     * @return UUID.
+     */
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    /**
      * Get ongoing set.
      * @return Playset.
      */
     public Playset getCurrentSet() {
-        return currentSet;
+        if (!sets.isEmpty()) {
+            int firstScore = sets.getLast().getPlayerOneSetScore();
+            int secondScore = sets.getLast().getPlayerTwoSetScore();
+            if (ScoreboardUtil.setScoreResult(firstScore, secondScore) == 0) {
+                return sets.getLast();
+            }
+        }
+        return null;
     }
 
     /** Action to start new set. */
     public void startNewSet() throws IOException {
-        currentSet = scoreboardService.playset().addNewPlayset(match);
+        Playset currentSet = scoreboardService.playset().addNewPlayset(match);
         System.out.println(currentSet + " is added. Start redirection.");
         FacesContext context = FacesContext.getCurrentInstance();
                 context
